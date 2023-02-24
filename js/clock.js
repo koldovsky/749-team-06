a1ab7a7e45dac9b07cc82502bd284ee69655318d
0lesuk <123981655+0lesuk@users.noreply.github.com>
2023-02-24 14:30:53 +0000

    created file js and added clock

---
+++ js/clock.js
@@ -0,0 +1,12 @@
+(() => {
+
+    function updateClock() {
+    const clockContainer = document.querySelector('.clock');
+    clockContainer.innerText = new Date().toLocaleTimeString();
+    }
+
+    setInterval(updateClock, 1000);
+
+
+
+})()
