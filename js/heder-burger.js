2d1a989efa6c960159b1abd2a084760a86aeabb2
Mygal-Alex <magalalecs2003@gmail.com>
2023-02-27 18:17:28 +0200

    completed all the small details in the header

--- js/heder-burger.js
+++ js/heder-burger.js
@@ -5,5 +5,13 @@
         document.querySelector('body').classList.toggle('overflow');
         document.querySelector('.header__menu').classList.toggle('burger-open');
         document.querySelector('.burger__contacts').classList.toggle('burger-open');
+        document.querySelector('main').classList.toggle('display');
+    })
+    document.querySelector('.list').addEventListener('click', function () {
+        document.querySelector('.burger').classList.toggle('active')
+        document.querySelector('.heder__burger-open').classList.toggle('open')
+        document.querySelector('body').classList.toggle('overflow');
+        document.querySelector('.header__menu').classList.toggle('burger-open');
+        document.querySelector('main').classList.toggle('display');
     })
 })()
