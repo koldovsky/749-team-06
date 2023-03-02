ef5ae0429b4e1a3b98394dfc2dfec158ebf0ca78
Blahyy <blahyyalex@gmail.com>
2023-03-02 19:03:47 +0200

    change 8 block

--- js/media-newspapers.js
+++ js/media-newspapers.js
@@ -1,21 +1,18 @@
 (function () {
-    const slides = document.querySelectorAll(".carousel__slide");
+    const slides = document.querySelectorAll(".press__media_carousel_slide");
     const nextBtn = document.querySelector('#next');
     const prevBtn = document.querySelector('#prev');
 
     let currentSlideInd = 0;
-    let secondSlideInd, thirdSlideInd;
+    let secondSlideInd;
 
     function renderSlide() {
         let slidesToShow = [slides[currentSlideInd]];
-        const slideContainer = document.querySelector(".carousel__slider");
+        const slideContainer = document.querySelector(".press__media_carousel_slider");
         if (window.innerWidth > 768) {
             secondSlideInd = currentSlideInd + 1 >= slides.length ? 0 : currentSlideInd + 1;
             slidesToShow.push(slides[secondSlideInd]);
-            if (window.innerWidth > 992) {
-                thirdSlideInd = secondSlideInd + 1 >= slides.length ? 0 : secondSlideInd + 1;
-                slidesToShow.push(slides[thirdSlideInd]);
-            }
+            
         }
         slideContainer.replaceChildren(...slidesToShow);
     }
